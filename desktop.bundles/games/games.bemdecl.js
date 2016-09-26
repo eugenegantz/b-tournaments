68703425e332c23bb3dc6b204758fb053de112d5
Eugene Gantz <eugengantz@gmail.com>
2016-09-26 19:06:23 +0400

    bundles

---
+++ desktop.bundles/games/games.bemdecl.js
@@ -0,0 +1,35 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "p-games"
+    },
+    {
+        "name": "b-games"
+    }
+];
---
+++ desktop.bundles/games/games.deps.js
@@ -0,0 +1,211 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "p-games"
+    },
+    {
+        "block": "m-api"
+    },
+    {
+        "block": "b-games"
+    },
+    {
+        "block": "checkbox"
+    },
+    {
+        "block": "checkbox",
+        "mod": "disabled"
+    },
+    {
+        "block": "checkbox",
+        "mod": "focused"
+    },
+    {
+        "block": "checkbox",
+        "mod": "checked"
+    },
+    {
+        "block": "checkbox",
+        "elem": "box"
+    },
+    {
+        "block": "checkbox",
+        "elem": "control"
+    },
+    {
+        "block": "checkbox",
+        "elem": "text"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "b-tabs"
+    },
+    {
+        "block": "i-events"
+    },
+    {
+        "block": "i-event"
+    },
+    {
+        "block": "m-tournament"
+    },
+    {
+        "block": "b-games",
+        "elem": "tournament"
+    },
+    {
+        "block": "i-bem",
+        "mod": "elem-instances"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "checkbox",
+        "mod": "disabled",
+        "val": true
+    },
+    {
+        "block": "checkbox",
+        "mod": "focused",
+        "val": true
+    },
+    {
+        "block": "checkbox",
+        "mod": "checked",
+        "val": true
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "m-default-data-model"
+    },
+    {
+        "block": "i-bem",
+        "mod": "elem-instances",
+        "val": true
+    },
+    {
+        "block": "m-games"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "elem-instances"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "elem-instances",
+        "val": true
+    }
+];
